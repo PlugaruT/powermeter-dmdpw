f0aad91cd6c9013f46ea1bd73702344ecbc6c5ce
PlugaruT <plugaru.tudor@gmail.com>
2016-06-02 17:28:07 +0300

    chache implementation added

--- powermeter/__init__.py
+++ powermeter/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
+from cachetools import cached
 
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import registers as reg
@@ -15,8 +16,22 @@ class PowerMeter(object):
         self.client.connect()
 
     def _get_holding_register_first(self, address, count):
+        """
+        Retrieve value from required register
+        :param address: starting address to read from
+        :param count: number of bytes to read
+        """
         raw = self.client.read_holding_registers(address, count, unit=SLAVE_ID)
         return raw.registers[0]
+
+    @cached(cache={})
+    def _get_dec_point(self):
+        """
+        Retrieve decimal point state, True for W, False for KW
+        """
+        value = self._get_holding_register_first(reg.DEC_POINT, 2)
+        value >>= 12
+        return (value & 0b0000) == 0b0000
 
     def get_voltage_value(self):
         """
@@ -43,10 +58,11 @@ class PowerMeter(object):
         Retrieve watt value, returns a float value
         """
         value = self._get_holding_register_first(reg.WATT, 2)
+        measure_unit = self._get_dec_point()
         # watt calculations
         # the value is computed as:
         # value/10
-        return value/10
+        return value/10 if measure_unit else value/10 * 1000
 
     def get_plus_wh(self):
         """
@@ -54,9 +70,10 @@ class PowerMeter(object):
         """
         high_wh = self._get_holding_register_first(reg.P_WH_HIGH, 2)
         low_wh = self._get_holding_register_first(reg.P_WH_LOW, 2)
+        measure_unit = self._get_dec_point()
         value = high_wh << 16
         value += low_wh
-        return value/10
+        return value/10 if measure_unit else value/10 * 1000
 
     def get_minus_wh(self):
         """
@@ -64,18 +81,10 @@ class PowerMeter(object):
         """
         high_wh = self._get_holding_register_first(reg.N_WH_HIGH, 2)
         low_wh = self._get_holding_register_first(reg.N_WH_LOW, 2)
+        measure_unit = self._get_dec_point()
         value = high_wh << 16
         value += low_wh
-        return value/10
-
-    def get_dec_point(self):
-        """
-        Retrieve decimal point state, True for W, False for KW
-        """
-        value = self._get_holding_register_first(reg.DEC_POINT, 2)
-        value >>= 12
-        return (value & 0b0000) == 0b0000
-
+        return value/10 if measure_unit else value/10 * 1000
 
     def get_relay_state(self):
         """

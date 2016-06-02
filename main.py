3fc73c0a11658b95943f2e53e0c958498d8c3305
PlugaruT <plugaru.tudor@gmail.com>
2016-06-02 17:31:04 +0300

    requirements updated

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
         watt = p.get_watt_value()
         p_wh = p.get_plus_wh()
         n_wh = p.get_minus_wh()
-        dec_point = p.get_dec_point()
         ry1, ry2, ry3, ry4 = p.get_relay_state()
         logging.info("Voltage: %s", voltage)
         logging.info("Ampere: %s", ampere)
@@ -24,5 +23,4 @@ if __name__ == '__main__':
         logging.info("-WH: %s", n_wh)
         logging.info("Relay State: RY1: %s, RY2: %s, RY3: %s, RY4: %s", ry1,
                      ry2, ry3, ry4)
-        logging.info("Decimal Point: %s", dec_point)
         sleep(1)
